40d495bd5386547d1fb225c31dbce97a683704b7
Alvin Ng <ngalvin93@gmail.com>
2019-11-08 16:48:14 -0600

    wip

--- comments/app.js
+++ comments/app.js
@@ -1,20 +1,24 @@
-let ProfilePic = (props) => {
-    return <span><img src={props.image}></img></span>
+// Make this component render an image
+const ProfilePic = () => {
+    return <span><img src="https://i.imgur.com/YcP0tik.jpg"></img></span>
 }
 
-let CommentBody = (props) => {
-    return <span><ProfilePic image="https://i.imgur.com/YcP0tik.jpg"/>Make this component render a username in bold above the comment text</span>
+// Make this component render a username in bold above the comment text
+const CommentBody = () => {
+    return <span><h4>Sharky McSharks</h4></span>
 }
 
-let Comment = (prop) => {
-    return <span><CommentBody/>Make each comment render a profile picture and the comment body</span>
+// Make each comment render a profile picture and the comment body
+const Comment = () => {
+    return <span>
+        <CommentBody/>
+        <ProfilePic/>
+        <p>Mystery snail lionfish papershell houndshark. Wobbegong shark blue whale gold damsel faucet snail.</p>
+        </span>
 }
 
-let Comments = () => {
-    return <span><li>
-        <Comment/>
-        <Comment/>
-    </li></span>
+const Comments = () => {
+    return (<span><Comment/></span>)
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
