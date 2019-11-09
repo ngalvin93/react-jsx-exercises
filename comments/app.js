5c41298fa1931c21b13cff14ac65dcd9a339c643
Alvin Ng <ngalvin93@gmail.com>
2019-11-08 21:50:26 -0600

    more

--- comments/app.js
+++ comments/app.js
@@ -1,24 +1,26 @@
 // Make this component render an image
-const ProfilePic = () => {
-    return <span><img src="https://i.imgur.com/YcP0tik.jpg"></img></span>
+const ProfilePic = (props) => {
+    return <span><img src={props.url}></img></span>
 }
 
 // Make this component render a username in bold above the comment text
-const CommentBody = () => {
-    return <span><h4>Sharky McSharks</h4></span>
+const CommentBody = (props) => {
+    return <span><h4>{props.username}</h4></span>
 }
 
 // Make each comment render a profile picture and the comment body
-const Comment = () => {
+const Comment = (props) => {
     return <span>
-        <CommentBody/>
-        <ProfilePic/>
-        <p>Mystery snail lionfish papershell houndshark. Wobbegong shark blue whale gold damsel faucet snail.</p>
+        <p>{props.comment}</p>
         </span>
 }
 
-const Comments = () => {
-    return (<span><Comment/></span>)
+const Comments = (props) => {
+    return (<span>
+        <ProfilePic url="https://i.imgur.com/YcP0tik.jpg"/>
+        <CommentBody username="Sharky McSharks"/>
+        <Comment comment="Mystery snail lionfish papershell houndshark. Wobbegong shark blue whale gold damsel faucet snail."/>
+        </span>)
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
--- first-component/app.js
+++ first-component/app.js
@@ -1,5 +1,10 @@
-let FirstComponent = () => {
-    return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+// Make this component render like the Windows logo
+let FirstComponent = (props) => {
+    return <span>
+        <a href="https://goo.gl/images/mY5Qdv" target="_blank">
+        <img src={props.url}/>
+        </a>
+        </span>
 }
 
-ReactDOM.render(<FirstComponent />, document.getElementById('root'))
+ReactDOM.render(<FirstComponent url="https://www.channelweb.co.uk/w-images/daa6fb74-bb2f-48c9-a399-464612d8252a/0/windowslogo-580x358.jpg"/>, document.getElementById('root'))
